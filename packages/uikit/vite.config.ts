85cd2d19f93edbdfb7f8ea86c04130454cac562d
Hayden Shively <hayden@morpho.xyz>
2025-03-19 18:15:31 -0500

    chore: cleanup uikit package

--- packages/uikit/vite.config.ts
+++ packages/uikit/vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ tsconfigPath: "./tsconfig.lib.json", rollupTypes: true, include: ["lib"] })],
+  plugins: [react(), dts({ tsconfigPath: "./tsconfig.package.json", rollupTypes: true })],
   build: {
     lib: {
-      entry: resolve(__dirname, "lib/main.ts"),
+      entry: resolve(__dirname, "src/main.ts"),
       formats: ["es"],
     },
     rollupOptions: {
@@ -16,7 +17,7 @@ export default defineConfig({
       output: {
         assetFileNames: "assets/[name][extname]",
         entryFileNames: "[name].js",
-      }
+      },
     },
     copyPublicDir: false,
   },
