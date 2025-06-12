07c15f93289873cf9bfd36c68cb49860fc7e1363
Hayden Shively <hayden@morpho.xyz>
2025-06-12 00:48:14 -0400

    chore: enable lisk, add katana, add tac

--- apps/fallback/src/lib/wagmi-config.ts
+++ apps/fallback/src/lib/wagmi-config.ts
@@ -47,10 +47,13 @@ function createFallbackTransport(rpcs: { url: string; batch: HttpTransportConfig
 }
 
 const chains = [
-  // core
+  // full support
   mainnet,
   base,
-  // other (alphabetical)
+  polygon,
+  unichain,
+  customChains.katana,
+  // lite support (alphabetical)
   arbitrum,
   // NOTE: Camp is disabled because RPC rate limits are too strict
   // customChains.basecamp,
@@ -64,11 +67,10 @@ const chains = [
   modeMainnet,
   optimism,
   plumeMainnet,
-  polygon,
   scrollMainnet,
   soneium,
   sonic,
-  unichain,
+  customChains.tac,
   worldchain,
 ] as const;
 
@@ -144,6 +146,10 @@ const transports: Record<(typeof chains)[number]["id"], Transport> = {
   [soneium.id]: createFallbackTransport(soneium.rpcUrls.default.http.map((url) => ({ url, batch: false }))),
   [customChains.hyperevm.id]: createFallbackTransport(
     customChains.hyperevm.rpcUrls.default.http.map((url) => ({ url, batch: false })),
+  ),
+  [customChains.katana.id]: createFallbackTransport(soneium.rpcUrls.default.http.map((url) => ({ url, batch: false }))),
+  [customChains.tac.id]: createFallbackTransport(
+    customChains.tac.rpcUrls.default.http.map((url) => ({ url, batch: false })),
   ),
   // [customChains.basecamp.id]: createFallbackTransport(
   //   customChains.basecamp.rpcUrls.default.http.map((url) => ({ url, batch: false })),
